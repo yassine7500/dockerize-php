7ba3340d6aa108674188a19ef993a43fce0533c3
Yassine El Garras <yassine7500@gmail.com>
2023-02-28 12:51:28 +0100

    change: remove the .git folder after cloning

--- run.sh
+++ run.sh
@@ -10,6 +10,8 @@ fi
 
 git clone https://github.com/yassine7500/dockerize-php.git
 cd dockerize-php
+#remove the .git folder
+rm -rf .git
 
 #check that the tag exists
 if git ls-remote --exit-code --tags origin "v$PHP_VERSION" > /dev/null 2>&1; then
